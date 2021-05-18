62cd36fdbad17b588510c39692aefc729628dc0d
Daniel <danichangt@gmail.com>
2021-05-17 23:08:58 -0600

    Query de DB, creacion y modificacion de tablas

--- ci_cunori_query.sql
+++ ci_cunori_query.sql
@@ -2,16 +2,17 @@ drop database if exists ci_cunori;
 create database ci_cunori;
 use ci_cunori;
 
+create table rol(
+	idrol int not null primary key auto_increment,
+    rol varchar(50) not null
+);
+
 create table usuario (
 	id int not null primary key auto_increment,
     usuario varchar(50) not null,
     clave varchar (25) not null,
-    rol_id int not null
-);
-
-create table roles (
-	id int not null primary key auto_increment,
-    rol varchar(50) not null
+    rol_id int not null,
+    foreign key (rol_id) references rol(idrol)
 );
 
 
@@ -35,9 +36,9 @@ create table articulo(
     no_clave_control varchar(45) not null, 
     descripcion longtext not null,
     valor decimal(12,2) not null,
-    fecha_ingreso timestamp,
-    categoria_codigo_control varchar(6) primary key,
-    tipo_idtipo int not null primary key,
+    fecha_ingreso date not null,
+    categoria_codigo_control varchar(6) not null,
+    tipo_idtipo int not null,
     activo tinyint not null default 1, 
     disponible tinyint not null default 1,
     foreign key (categoria_codigo_control) references categoria(codigo_control),
@@ -50,8 +51,8 @@ create table empleado(
     codigo varchar(45),
     nombre varchar(45),
     apellidos varchar(45),
-    areaemp_idarea tinyint not null primary key,
-    foreign key (area_idarea) references areaemp(idearea)
+    areaemp_idarea tinyint not null,
+    foreign key (areaemp_idarea) references areaemp(idarea)
 );
 
 
@@ -59,10 +60,10 @@ create table asignacion(
 	idasignacion int not null primary key auto_increment,
     tarjeta_responsable mediumint null,
     fecha_asignacion timestamp not null,
-    articulo_idarticulos int not null primary key, 
+    articulo_idarticulo int not null, 
     estado tinyint not null default 1,
-    empleado_idempleado tinyint not null primary key,
-    foreign key (articulo_idarticulo) references articulo(id_articulo),
+    empleado_idempleado smallint not null,
+    foreign key (articulo_idarticulo) references articulo(idarticulo),
     foreign key (empleado_idempleado) references empleado(idempleado)
 );
 
@@ -75,6 +76,6 @@ create table mantenimiento_vehiculo(
 );
 
 
-alter table usuario add foreign key (rol_id) references roles(id);
-insert into roles (rol) values ('Administrador');
-insert into roles (rol) values ('Lector');
+insert into rol (rol) values ('Administrador');
+insert into rol (rol) values ('Lector');
+
