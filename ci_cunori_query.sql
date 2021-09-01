5e94a8c95c4dca3a47d0f2602588af4deb01a273
Daniel <danichangt@gmail.com>
2021-09-01 00:22:35 -0600

    Actualizacion de diagramas y archivo de querys

--- ci_cunori_query.sql
+++ ci_cunori_query.sql
@@ -3,80 +3,114 @@ create database ci_cunori;
 use ci_cunori;
 
 create table rol(
-	idrol int not null primary key auto_increment,
-    rol varchar(50) not null
+	idrol smallint not null primary key auto_increment,
+    rol varchar(50) not null unique,
+    crear tinyint not null default 0,
+    leer tinyint not null default 0,
+    editar tinyint not null default 0,
+    eliminar tinyint not null default 0
 );
 
-create table usuario (
-	id int not null primary key auto_increment,
-    usuario varchar(50) not null,
-    clave varchar (25) not null,
-    rol_id int not null,
-    foreign key (rol_id) references rol(idrol)
+insert into rol (rol, crear, leer, editar, eliminar) values ('Administrador', 1, 1, 1, 1);
+insert into rol (rol, crear, leer, editar, eliminar) values ('Lector', 0, 1, 0, 0);
+
+create table usuario(
+	idusuario smallint not null primary key auto_increment,
+    usuario varchar(50) not null unique,
+    clave text not null,
+    nombres varchar(50) not null, 
+    apellidos varchar(50) not null,
+    estado tinyint not null default 0,
+    rol_idrol smallint not null,
+    foreign key (rol_idrol) references rol(idrol) on update cascade
 );
 
+create table token(
+	idtoken tinyint not null primary key auto_increment,
+	usuario varchar(50) not null unique,
+    token text not null
+);
 
-create table categoria (
-	codigo_control varchar(6) not null primary key unique,
+create table categoria(
+	idcategoria smallint not null primary key auto_increment,
+	codigo_control varchar(6) not null unique,
     descripcion varchar(100) not null
 );
 
 create table tipo(
-	idtipo int not null primary key auto_increment,
-    descripcion varchar(25) not null
+	idtipo smallint not null primary key auto_increment,
+    descripcion varchar(50) not null
 );
 
 create table areaemp(
 	idarea tinyint not null primary key auto_increment,
-    descripcion varchar(45) not null
+    descripcion varchar(50) not null
 );
 
 create table articulo(
 	idarticulo int not null  primary key auto_increment,
-    no_clave_control varchar(45) not null, 
+    no_clave_control varchar(50) not null, 
     descripcion longtext not null,
     valor decimal(12,2) not null,
     fecha_ingreso date not null,
-    categoria_codigo_control varchar(6) not null,
-    tipo_idtipo int not null,
+    folio mediumint not null,
     activo tinyint not null default 1, 
     disponible tinyint not null default 1,
-    foreign key (categoria_codigo_control) references categoria(codigo_control),
-    foreign key (tipo_idtipo) references tipo(idtipo) on delete cascade
+    categoria_idcategoria smallint not null,
+    tipo_idtipo smallint not null,
+    foreign key (categoria_idcategoria) references categoria(idcategoria) on update cascade,
+    foreign key (tipo_idtipo) references tipo(idtipo) on update cascade
 );
 
 create table empleado(
 	idempleado smallint not null auto_increment primary key,
     dpi varchar(20) not null,
     codigo varchar(45),
-    nombre varchar(45),
+    nombres varchar(45),
     apellidos varchar(45),
     areaemp_idarea tinyint not null,
-    foreign key (areaemp_idarea) references areaemp(idarea)
+    foreign key (areaemp_idarea) references areaemp(idarea) on update cascade
 );
 
 
 create table asignacion(
 	idasignacion int not null primary key auto_increment,
     tarjeta_responsable mediumint null,
-    fecha_asignacion timestamp not null,
-    articulo_idarticulo int not null, 
+    fecha_asignacion date not null,
     estado tinyint not null default 1,
+    localizacion varchar(100) not null,
+    autorizacion varchar(50) null,
+    seccion text null,
+    contador_traslado tinyint not null default 1,
+    articulo_idarticulo int not null, 
     empleado_idempleado smallint not null,
-    foreign key (articulo_idarticulo) references articulo(idarticulo),
-    foreign key (empleado_idempleado) references empleado(idempleado)
+    asignacion_idasignacion int null, 
+    foreign key (articulo_idarticulo) references articulo(idarticulo) on update cascade,
+    foreign key (empleado_idempleado) references empleado(idempleado) on update cascade,
+    foreign key (asignacion_idasignacion) references asignacion(idasignacion) on update cascade
 );
 
-create table mantenimiento_vehiculo(
+create table mantenimiento(
 	idmantenimiento int not null auto_increment primary key,
-    fecha_mantenimiento timestamp not null,
+    fecha_mantenimiento date not null,
     descripcion text not null,
-    no_factura varchar(45) null,
-    valor_neto decimal(10,2) not null
+    no_factura varchar(50) null,
+    valor_neto decimal(10,2) not null,
+    asignacion_idasignacion int not null,
+    foreign key (asignacion_idasignacion) references asignacion(idasignacion) on update cascade
 );
 
 
-insert into rol (rol) values ('Administrador');
-insert into rol (rol) values ('Lector');
-
-delete from tipo where idtipo = 1
+insert into categoria (codigo_control, descripcion) values('24-A', 'Equipo educacional, cultura y recreativo'),
+														('24-B', 'Mobiliario y equipo médico-sanitario y de laboratorio'),
+                                                        ('24-C', 'Mobiliario y equipo de oficina'),
+                                                        ('24-D', 'Equipo de computación'),
+                                                        ('24-E', 'Obras de arte'),
+                                                        ('24-F', 'Equipo militar y de seguridad'),
+                                                        ('24-G', 'Animales'),
+                                                        ('24-H', 'Libros, revistas y otros elementos coleccionables'),
+                                                        ('24-I', 'Equipo para comunicaciones'),
+                                                        ('24-M', 'Equipo de transporte'),
+                                                        ('24-O', 'Maquinaria y equipo de producción'),
+                                                        ('24-P', 'Maquinaria y equipo para la construcción'),
+                                                        ('24-Z', 'Otras máquinas y equipos');
